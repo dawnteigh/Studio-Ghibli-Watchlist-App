df414d268ae24d71794fdd17467de251f9713af5
Donte Ladatto <donte279@gmail.com>
2022-03-23 17:17:13 -0500

    successfully populate /watchlist with data from API

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //Global Variables
 const BASE_URL = "https://ghibliapi.herokuapp.com/films"
+const LOCAL_URL = "http://localhost:3000"
 const sidebar = document.querySelector("#list")
 const display = document.querySelector("#show-panel")
 
@@ -25,10 +26,36 @@ function displayFilm(e) {
     fetch(BASE_URL + `/${e.target.id}`)
     .then(res => res.json())
     .then(data => {
+        let image = data.movie_banner
+        let title = data.title
+        let year = data.release_date
+        let desc = data.description
         display.innerHTML = 
-        `<img class="resize" src="${data.movie_banner}">
-        <h1>${data.title} <span class="year">${data.release_date}</span></h1>
-        <p>${data.description}</p>`
+        `<img class="resize" src="${image}">
+        <h1>${title} <span class="year">${year}</span></h1>
+        <p>${desc}</p>
+        <button class="watch">Add to Watchlist</button>
+        <button class="favorite">Add to Favorites</button>`
 
+        display.addEventListener('click', (e) => {
+                if (e.target.className === "watch") {
+                    fetch(LOCAL_URL + '/watchlist', {
+                        method: 'POST',
+                        headers: {
+                            "Content-Type": "application/json",
+                            Accept: "application/json"
+                          },
+                      
+                          body: JSON.stringify({
+                            "movie_banner": image,
+                            "title": title,
+                            "release_date": year,
+                            "description": desc
+                          })
+                        
+                    })
+                }
+            })
     })
+
 }
