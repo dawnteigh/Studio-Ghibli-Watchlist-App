cae131580376930dda85b8ebf05b597caec782c1
Donte Ladatto <donte279@gmail.com>
2022-03-23 15:59:12 -0500

    sidebar click event, image resize css

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Global Variables
 const BASE_URL = "https://ghibliapi.herokuapp.com/films"
 const sidebar = document.querySelector("#list")
+const display = document.querySelector("#show-panel")
 
 //Startup routine
 document.addEventListener("DOMContentLoaded", () => {
@@ -9,10 +10,21 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(data => data.forEach(film => getFilms(film))
 )})
 
+//Event Listeners
+sidebar.addEventListener("click", displayFilm)
+
 //Callback functions
 function getFilms(film) {
     const liElement = document.createElement("li")
     liElement.innerHTML =
     `<a id="${film.id}">${film.title}</a>`
     sidebar.append(liElement)
+}
+
+function displayFilm(e) {
+    fetch(BASE_URL + `/${e.target.id}`)
+    .then(res => res.json())
+    .then(data => {
+        display.innerHTML = `<img class="resize" src="${data.movie_banner}">`
+    })
 }
