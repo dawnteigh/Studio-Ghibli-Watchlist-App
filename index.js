e161cf4c9ce1f9c458df76da68213a3d9a11fd47
Donte Ladatto <donte279@gmail.com>
2022-03-26 11:37:40 -0500

    films are now posted with an ID, duplicates throw an error w/ alerts

--- index.js
+++ index.js
@@ -18,6 +18,18 @@ document.addEventListener("DOMContentLoaded", () => {
 sidebar.addEventListener("click", displayFilm)
 wButton.addEventListener("click", displayList)
 fButton.addEventListener("click", displayList)
+//Delete functionality
+document.addEventListener("click", (e) => {
+    if (e.target.className === "delete") {
+        fetch(LOCAL_URL + `${e.target.id}`, {
+            method: "DELETE"
+        })
+            .then(res => res.json())
+            .then(res => {
+                e.target.parentElement.remove()
+            })
+    }
+})
 
 //Callback functions
 function getFilms(film) {
@@ -36,37 +48,46 @@ function displayFilm(e) {
             let title = data.title
             let year = data.release_date
             let desc = data.description
+            let eyeD = data.id
             display.innerHTML =
                 `<img class="resize" src="${image}">
         <h1>${title} <span class="year">${year}</span></h1>
         <p class="width">${desc}</p>
         <button class="watch">Add to Watchlist</button>
         <button class="favorite">Add to Favorites</button>`
-           
+
             const addWatch = display.querySelector(`.watch`)
             const addFav = display.querySelector(`.favorite`)
             const postConfig = {
                 method: 'POST',
                 headers: {
                     "Content-Type": "application/json",
-                    Accept: "application/json"
+                    Accept: "application/json",
+
                 },
 
                 body: JSON.stringify({
                     "movie_banner": image,
                     "title": title,
                     "release_date": year,
-                    "description": desc
+                    "description": desc,
+                    "id": eyeD
                 })
             }
             addWatch.addEventListener('click', (e) => {
                 fetch(LOCAL_URL + '/watchlist', postConfig)
+                .then(res => res.json())
+                .catch(error => alert(`${title} is already on your watchlist!`))
+                e.target.disabled = true
             })
             addFav.addEventListener('click', (e) => {
                 fetch(LOCAL_URL + '/favorites', postConfig)
+                .then(res => res.json())
+                .catch(error => alert(`${title} is already in your favorites!`))
+                e.target.disabled = true
             })
 
-
+            //try a fetch to the ID, then post if not found
         })
 
 }
@@ -94,16 +115,3 @@ function displayList(e) {
         }))
 }
 
-//delete button event listener
-document.addEventListener("click", (e) => {
-    if (e.target.className === "delete") {
-        fetch(LOCAL_URL + `${e.target.id}`, {
-            method: "DELETE"
-        })
-            .then(res => res.json())
-            .then(res => {
-                e.target.parentElement.remove()
-            })
-    }
-
-})
