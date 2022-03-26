910b467d72475c98bfbdded136eac9dba2a6a5db
Donte Ladatto <donte279@gmail.com>
2022-03-25 21:44:08 -0500

    inelegant execute once solution, last snapshot before separate event listeners

--- index.js
+++ index.js
@@ -39,15 +39,15 @@ function displayFilm(e) {
         `<img class="resize" src="${image}">
         <h1>${title} <span class="year">${year}</span></h1>
         <p class="width">${desc}</p>
-        <button class="watch">Add to Watchlist</button>
-        <button class="favorite">Add to Favorites</button>`
+        <button class="${data.id}-watch">Add to Watchlist</button>
+        <button class="${data.id}-favorite">Add to Favorites</button>`
         //the event listeners are sticking around, a new one is created every time a new film is clicked
         display.addEventListener('click', (e) => {
             let endpoint;
-                if (e.target.className === "watch") {
+                if (e.target.className === `${data.id}-watch`) {
                     endpoint = "/watchlist"
                 }
-                else if (e.target.className === "favorite") {
+                else if (e.target.className === `${data.id}-favorite`) {
                     endpoint = "/favorites"
                 }//Making a post to favorites and then making a different post to watchlist results in two posts to watchlist
                 console.log(e.target)
@@ -66,7 +66,8 @@ function displayFilm(e) {
                           })
                         
                     })
-                }
+                    e.target.disabled = true
+                 }, {once: true}
         )
     })
 
@@ -76,7 +77,7 @@ function displayList(e) {
     display.innerHTML = ""
     let endpoint;
     let header;
-    
+
     if (e.target.id === "watchlist") {
         endpoint = "/watchlist"
         header = "My Watchlist"
@@ -87,22 +88,24 @@ function displayList(e) {
     }
     display.innerHTML = `<h2>${header}</h2>`
     fetch(LOCAL_URL + endpoint)
-    .then(res => res.json())
-    .then(data => data.forEach(film => {
-        display.innerHTML += `<div class="card"><img class="thumbnail" src="${film.movie_banner}"><br>
+        .then(res => res.json())
+        .then(data => data.forEach(film => {
+            display.innerHTML += `<div class="card"><img class="thumbnail" src="${film.movie_banner}"><br>
         <h3>${film.title}</h3><button class="delete" id="${film.id}">Remove from List</button><br><br></div>`
     }))
     //endpoint is not being changed here, both end points are being accessed
     display.addEventListener("click", (e) => {
-    if (e.target.className === "delete") {
-        fetch(LOCAL_URL + endpoint + `/${e.target.id}`, {
-          method: "DELETE"
-        })
-        .then(res => res.json())
-        .then(res => {
-            e.target.parentElement.remove()
-            console.log(endpoint)
-          })
-      }
+        if (e.target.className === "delete") {
+            fetch(LOCAL_URL + endpoint + `/${e.target.id}`, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(res => {
+                    e.target.parentElement.remove()
+                    console.log(endpoint)
+                })
+        }
+        
     })
+
 }
