69b175efafc5edd7731c0d758297ba791b129e30
Donte Ladatto <donte279@gmail.com>
2022-03-30 18:00:57 -0500

    Empty lists now display a message

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const sidebar = document.querySelector("#list-panel")
 const display = document.querySelector("#show-panel")
 const wButton = document.querySelector("#watchlist")
 const fButton = document.querySelector("#favslist")
+const empty = document.querySelector(".empty")
 
 //Startup routine
 document.addEventListener("DOMContentLoaded", () => {
@@ -14,7 +15,6 @@ document.addEventListener("DOMContentLoaded", () => {
         )
         .catch(error => {
             alert("Failed to load list of films, check your internet connection and refresh to try again.");
-            console.log(`Failed to fetch data from ${BASE_URL}.`)
         })
 })
 
@@ -22,6 +22,7 @@ document.addEventListener("DOMContentLoaded", () => {
 sidebar.addEventListener("click", displayFilm)
 wButton.addEventListener("click", displayList)
 fButton.addEventListener("click", displayList)
+
 //Delete functionality
 document.addEventListener("click", (e) => {
     if (e.target.className === "delete") {
@@ -47,54 +48,54 @@ function getFilms(film) {
 //callback for sidebar event listener
 function displayFilm(e) {
     if (e.target.className === "film") {
-    display.innerHTML = ""
-    fetch(BASE_URL + `/${e.target.id}`)
-        .then(res => res.json())
-        .then(data => {
-            let image = data.movie_banner
-            let title = data.title
-            let year = data.release_date
-            let desc = data.description
-            let eyeD = data.id
-            display.innerHTML =
-                `<img class="resize" src="${image}">
+        display.innerHTML = ""
+        fetch(BASE_URL + `/${e.target.id}`)
+            .then(res => res.json())
+            .then(data => {
+                let image = data.movie_banner
+                let title = data.title
+                let year = data.release_date
+                let desc = data.description
+                let eyeD = data.id
+                display.innerHTML =
+                    `<img class="resize" src="${image}">
         <h1>${title} <span class="year">${year}</span></h1>
         <p class="width">${desc}</p>
         <button class="watch">Add to Watchlist</button>
         <button class="favorite">Add to Favorites</button>`
 
-            const addWatch = display.querySelector(`.watch`)
-            const addFav = display.querySelector(`.favorite`)
-            const postConfig = {
-                method: 'POST',
-                headers: {
-                    "Content-Type": "application/json",
-                    Accept: "application/json",
+                const addWatch = display.querySelector(`.watch`)
+                const addFav = display.querySelector(`.favorite`)
+                const postConfig = {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json",
+                        Accept: "application/json",
 
-                },
+                    },
 
-                body: JSON.stringify({
-                    "movie_banner": image,
-                    "title": title,
-                    "release_date": year,
-                    "description": desc,
-                    "id": eyeD
+                    body: JSON.stringify({
+                        "movie_banner": image,
+                        "title": title,
+                        "release_date": year,
+                        "description": desc,
+                        "id": eyeD
+                    })
+                }
+                addWatch.addEventListener('click', (e) => {
+                    fetch(LOCAL_URL + '/watchlist', postConfig)
+                        .then(res => res.json())
+                        .catch(error => alert(`${title} is already on your watchlist!`))
+                    e.target.disabled = true
                 })
-            }
-            addWatch.addEventListener('click', (e) => {
-                fetch(LOCAL_URL + '/watchlist', postConfig)
-                .then(res => res.json())
-                .catch(error => alert(`${title} is already on your watchlist!`))
-                e.target.disabled = true
+                addFav.addEventListener('click', (e) => {
+                    fetch(LOCAL_URL + '/favorites', postConfig)
+                        .then(res => res.json())
+                        .catch(error => alert(`${title} is already in your favorites!`))
+                    e.target.disabled = true
+                })
             })
-            addFav.addEventListener('click', (e) => {
-                fetch(LOCAL_URL + '/favorites', postConfig)
-                .then(res => res.json())
-                .catch(error => alert(`${title} is already in your favorites!`))
-                e.target.disabled = true
-            })
-        })
-        .catch(error => alert("Could not complete your request. Check your connection and try again."))
+            .catch(error => alert("Could not complete your request. Check your connection and try again."))
     }
 }
 
@@ -115,9 +116,16 @@ function displayList(e) {
     display.innerHTML = `<h2>${header}</h2>`
     fetch(LOCAL_URL + endpoint)
         .then(res => res.json())
-        .then(data => data.forEach(film => {
-            display.innerHTML += `<div class="card"><img class="thumbnail" src="${film.movie_banner}"><br>
+        .then(data => {
+            if (data.length === 0) {
+                display.innerHTML += `<p class="empty">You haven't added anything to this list yet; select a film and get started!</p>`
+            }
+            else {
+                data.forEach(film => {
+                    display.innerHTML += `<div class="card"><img class="thumbnail" src="${film.movie_banner}"><br>
         <h3>${film.title}</h3><button class="delete" id="${endpoint + "/" + film.id}">Remove from List</button><br><br></div>`
-        }))
+                })
+            }
+        })
 }
 
